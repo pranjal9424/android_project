bc022c5360372157cb64d3b3b421644e4edc5d30
pranjal9424 <pranjalgupta9424@gmail.com>
2019-08-09 12:07:35 +0530

    my third commit

--- Ecommerce/app/src/main/java/com/example/ecommerce/MainActivity.java
+++ Ecommerce/app/src/main/java/com/example/ecommerce/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.ecommerce;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button joinNowButton,loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        joinNowButton=(Button)findViewById(R.id.main_join_btn);
+        loginButton=(Button)findViewById(R.id.main_login_btn);
     }
 }
